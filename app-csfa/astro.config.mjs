f4c1878cf86b413a852230a1f40ed10d4760c201
Cloves-CSFA <cvr.neto20@gmail.com>
2024-11-21 18:08:08 -0300

    feat: add dependencias

---
+++ app-csfa/astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import node from '@astrojs/node';
+
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import icon from 'astro-icon';
+
+import netlify from '@astrojs/netlify';
+
+import alpinejs from '@astrojs/alpinejs';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+
+  adapter: netlify(),
+
+  integrations: [tailwind(), react(), icon(), alpinejs()]
+});
