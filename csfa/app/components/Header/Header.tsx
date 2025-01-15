3bb5642ed210cb0de46b9250f9c7402561b3866d
Cloves-CSFA <cvr.neto20@gmail.com>
2025-01-15 18:39:30 -0300

    feat: alterando padding header

--- csfa/app/components/Header/Header.tsx
+++ csfa/app/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header(){
 
   return(
 
-      <header className='fixed z-50 top-0 left-0 w-full h-32 p-2.5'>
+      <header className='fixed z-50 top-0 left-0 w-full h-32 p-2'>
           <Navgation />
       </header>
 
