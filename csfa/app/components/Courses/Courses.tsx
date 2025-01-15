67568ed7fcb64bbe48d2b9596d7a67dae85087db
Cloves-CSFA <cvr.neto20@gmail.com>
2025-01-15 18:38:54 -0300

    feat: responsive courses

--- csfa/app/components/Courses/Courses.tsx
+++ csfa/app/components/Courses/Courses.tsx
@@ -1,5 +1,5 @@
 
-"use client";
+'use client';
 
 import { Card } from "flowbite-react";
 import { Icon } from '@iconify/react';
@@ -42,22 +42,39 @@ const courses = [
   return (
 
     <>
-        <section className=" w-dvw h-dvh flex justify-center space-x-4 gap-4 p-6">
-            {courses.map((item) => (
-                <Card className="w-72 h-72 relative flex items-end justify-start">
-                    <img src={item.src} alt={item.alt} className="absolute left-0 top-0 w-full h-full object-cover"/>
+        {courses.map((item, index) => (
+                <Card key={index} className= "relative bg-blue-600 w-[60%] h-64 flex items-end justify-start overflow-hidden xl:w-64 xl:h-64">
+                    <img src={item.src} alt={item.alt} className="absolute left-0 top-0 w-full h-full object-cover 
+                                                                md:w-[50%]
+                                                                xl:w-full
+                                                                "/>
                     
-                    <div className="absolute flex items-center justify-center z-10 top-2 right-2 rounded-full w-12 h-12 bg-white">
-                        <Icon className="rotate-90 text-2xl" icon="material-symbols:arrow-insert-rounded" />
+                    <div className="absolute flex items-center justify-center z-10 top-2 right-2 rounded-full w-12 h-12 bg-white border">
+                        <Icon className="rotate-90 text-2xl text-blue-600" icon="material-symbols:arrow-insert-rounded" />
                     </div>
 
-                    <div className="absolute left-0 bottom-0 w-full flex-col gap-2 p-2 text-white">
-                        <h2 className="font-semibold">{item.title}</h2>
-                        <p className="font-normal">{item.description}</p>
+                    <div className="absolute left-0 bottom-0 w-full flex-co gap-2 p-2.5 space-y-2 text-white bg-gradient-to-t from-blue-950 via-blue-900/90 to-blue-800/rounded-b-lg 
+                                    md:w-[50%] 
+                                    md:h-full 
+                                    md:flex-col
+                                    md:left-[50%] 
+                                    md:rounded-none 
+                                    md:justify-center md:flex 
+                                    md:p-8 
+                                    xl:p-2.5
+                                    xl:object-cover
+                                    xl:absolute 
+                                    xl:left-0 
+                                    xl:bottom-0 
+                                    xl:w-full 
+                                    xl:flex-col 
+                                    xl:h-auto"
+                                    >
+                        <h2 className="font-bold text-xl">{item.title}</h2>
+                        <p className="font-light">{item.description}</p>
                     </div>
                 </Card>
             ))}
-        </section>
         
     </>
     
---
+++ csfa/app/components/Courses/SectionCourses.tsx
@@ -0,0 +1,18 @@
+import Title from "../Title/Title";
+import '@/styles/Title.css';
+import Courses from "./Courses";
+
+
+export default function SectionCourses(){
+   return(
+        <section className="w-full h-auto bg-blue-700 flex flex-col justify-center items-center p-6 space-y-4">
+            
+            <Title title="Segmentos" />
+
+            <div className="w-dvw h-auto flex flex-col flex-wrap items-center justify-center p-6 space-y-6 md:space-y-0 md:gap-4 md:p-6 xl:flex-row">
+                <Courses />
+            </div>
+            
+        </section>
+   )
+}
