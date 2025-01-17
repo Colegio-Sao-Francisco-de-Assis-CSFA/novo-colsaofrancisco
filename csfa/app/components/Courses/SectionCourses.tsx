ab1ca26a99363573c61a8170562f38431676507d
Cloves-CSFA <cvr.neto20@gmail.com>
2025-01-17 19:10:16 -0300

    refactore: courses responsivo corrigido

--- csfa/app/components/Courses/SectionCourses.tsx
+++ csfa/app/components/Courses/SectionCourses.tsx
@@ -5,9 +5,9 @@ import Courses from "./Courses";
 
 export default function SectionCourses(){
    return(
-        <section className="w-full h-auto bg-blue-700 flex flex-col justify-center items-center p-6 space-y-4">
+        <section className="w-full h-auto bg-blue-600 flex flex-col justify-center items-center p-12 space-y-4">
             
-            <Title title="Segmentos" />
+            <Title title="Segmentos" className="text-white" />
 
             <div className="w-dvw h-auto flex flex-col flex-wrap items-center justify-center p-6 space-y-6 md:space-y-0 md:gap-4 md:p-6 xl:flex-row">
                 <Courses />
