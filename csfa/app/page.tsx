3f840e0411ac27b3526f44b5a5f959348cc7530f
Cloves-CSFA <cvr.neto20@gmail.com>
2025-01-20 18:46:00 -0300

    att: packages

--- csfa/app/page.tsx
+++ csfa/app/page.tsx
@@ -3,17 +3,17 @@ import SectionHeroCarousel from "./components/Hero/SectionHero";
 import SectionCourses from "./components/Courses/SectionCourses";
 import SectionAbout from "./components/About/About";
 import SectionNews from "./components/News/SectionNews";
-import SectionContact from "./components/Contact/Form";
+import SectionContact from "./components/Contact/SectionContact";
 import SectionFeatures from "./components/Features/SectionFeatures";
 import Footer from "./components/Footer/Footer";
 
 export default function PaginaInicial() {
   return (
       <>
-        <SectionHeroCarousel/>
-        <SectionCourses />
-        <SectionAbout/>
-        <SectionFeatures/>
+        {/* <SectionHeroCarousel/> */}
+        {/* <SectionCourses /> */}
+        {/* <SectionAbout/> */}
+        {/* <SectionFeatures/> */}
         <SectionNews/>
         <SectionContact/>
         <Footer/>
